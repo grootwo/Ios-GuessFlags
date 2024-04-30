8c1cd0d693806570e78d8246430c035808391f7f
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-04-30 16:29:46 +0900

    Create Alert

--- GuessFlags/GuessFlags/ContentView.swift
+++ GuessFlags/GuessFlags/ContentView.swift
@@ -8,38 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
         VStack(spacing: 20) {
-            Button("default") { }
-                .buttonStyle(.automatic)
-            Button("plain") { }
-                .buttonStyle(.plain)
-            Button("bordered") { }
-                .buttonStyle(.bordered)
-                .tint(.purple)
-            Button("borderedProminent") { }
-                .buttonStyle(.borderedProminent)
-            Button("borderless") { }
-                .buttonStyle(.borderless)
-            Button(action: {}, label: {
-                Text("Custom Button")
-                    .padding(20)
-                    .foregroundColor(.white)
-                    .background(.orange)
-            })
-            Button("Save", systemImage: "square.and.arrow.down") {}
-                .padding(20)
-                .foregroundColor(.white)
-                .background(.orange)
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Label("record", systemImage: "mic.circle.fill")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .padding(20)
-                    .foregroundColor(.white)
-                    .background(.orange)
-            })
+            Button("Show Alert") {
+                showingAlert = true
+            }
+            .buttonStyle(.bordered)
         }
         .tint(.green)
+        .alert("Alert Message", isPresented: $showingAlert) {
+            Button("Back", role: .cancel) {}
+            Button("Delete", role: .destructive) {}
+        } message: {
+            Text("This is message for alert")
+        }
     }
 }
 
