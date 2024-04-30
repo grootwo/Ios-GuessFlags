89655262bcb9da32e9b24d4007bd3104369b8489
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-04-30 15:56:36 +0900

    Create gradients

--- GuessFlags/GuessFlags/ContentView.swift
+++ GuessFlags/GuessFlags/ContentView.swift
@@ -9,17 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.primary
-                Color.secondary
-            }
-            Text("This is a Text")
-                .padding(30)
-                .background(.ultraThinMaterial)
+        VStack {
+            LinearGradient(stops: [
+                Gradient.Stop(color: .white, location: 0.2),
+                Gradient.Stop(color: .blue, location: 0.4)
+            ], startPoint: .top, endPoint: .bottom)
+            LinearGradient(stops: [
+                .init(color: .red, location: 0.2),
+                .init(color: .cyan, location: 0.8)
+            ], startPoint: .leading, endPoint: .trailing)
+            RadialGradient(colors: [.brown, .orange, .mint], center: .center, startRadius: 20, endRadius: 150)
+            AngularGradient(colors: [.red, .yellow, .blue, .red], center: .center)
+            Text("These are gradient")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+                .foregroundStyle(.white)
+                .background(.green.gradient)
         }
-        .ignoresSafeArea()
-        
     }
 }
 
