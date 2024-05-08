2e8365a3131eae87d6faa07a60b4334f3d16c89b
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-08 17:43:49 +0900

    Set situation and func guessAnswer

--- GuessRockScissorsPaper/GuessRockScissorsPaper/ContentView.swift
+++ GuessRockScissorsPaper/GuessRockScissorsPaper/ContentView.swift
@@ -10,21 +10,42 @@ import SwiftUI
 struct ContentView: View {
     let choices = ["Rock", "Scissors", "Paper"]
     let choicesEmoji = ["✊", "✌️", "✋"]
-    @State var selectedChoice = Int.random(in: 0...2)
+    let situations = ["WIN", "LOSE"]
+    @State var pickedChoice = Int.random(in: 0...2)
+    @State var pickedSituation = Int.random(in: 0...1)
+    @State var answer = ""
     var body: some View {
-        VStack {
-            Text(choicesEmoji[selectedChoice])
+        VStack(spacing: 30) {
+            Text(choicesEmoji[pickedChoice])
                 .font(.system(size: 200))
-            Text("What is your choice to win?")
+            Text("What is your choice to \(situations[pickedSituation])?")
+                .font(.title)
+                .fontWeight(.bold)
             HStack {
-                ForEach(choicesEmoji, id: \.self) { choice in
+                ForEach(0..<3) { choice in
                     Button(action: {
                         print("\(choice) is clicked")
                     }, label: {
-                        Text(choice)
+                        Text(choicesEmoji[choice])
                             .font(.system(size: 100))
                     })
                 }
+            }
+        }
+    }
+    
+    func guessAnswer(selectedChoice: Int) {
+        if pickedSituation == 0 {
+            if pickedChoice == 0 && selectedChoice == 2 || pickedChoice == 1 && selectedChoice == 0 || pickedChoice == 2 && selectedChoice == 1 {
+                answer = "Correct"
+            } else {
+                answer = "Wrong"
+            }
+        } else {
+            if pickedChoice == 0 && selectedChoice == 1 || pickedChoice == 1 && selectedChoice == 2 || pickedChoice == 2 && selectedChoice == 0 {
+                answer = "Correct"
+            } else {
+                answer = "Wrong"
             }
         }
     }
