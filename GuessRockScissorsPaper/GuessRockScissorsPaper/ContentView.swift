50154ff8a464370683c2ef986302c8b97c3d955f
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-08 18:07:26 +0900

    Set number of questions and show its value differently

--- GuessRockScissorsPaper/GuessRockScissorsPaper/ContentView.swift
+++ GuessRockScissorsPaper/GuessRockScissorsPaper/ContentView.swift
@@ -16,8 +16,12 @@ struct ContentView: View {
     @State var answer = ""
     @State var isShowingAlert = false
     @State var score = 0
+    @State var numberOfQuestion = 1
     var body: some View {
         VStack(spacing: 30) {
+            Text("\(numberOfQuestion) / 10")
+                .font(.title3)
+            Spacer()
             Text(choicesEmoji[pickedChoice])
                 .font(.system(size: 200))
             Text("What is your choice to \(situations[pickedSituation])?")
@@ -35,12 +39,18 @@ struct ContentView: View {
                     })
                 }
             }
+            Spacer()
+            Text("Your score: \(score)")
         }
         .padding()
         .alert(answer, isPresented: $isShowingAlert) {
             Button("Continue", action: setNewQuestion)
         } message: {
-            Text("Now your score is \(score)!")
+            if numberOfQuestion == 10 {
+                Text("Your total score is \(score)!!!")
+            } else {
+                Text("Now your score is \(score)!")
+            }
         }
     }
     
@@ -67,6 +77,11 @@ struct ContentView: View {
     func setNewQuestion() {
         pickedChoice = Int.random(in: 0...2)
         pickedSituation = Int.random(in: 0...1)
+        numberOfQuestion += 1
+        if numberOfQuestion == 11 {
+            numberOfQuestion = 0
+            score = 0
+        }
     }
 }
 
